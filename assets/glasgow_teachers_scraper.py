b96273bb015d821cfc683a3135f09450c7c46fc8
hkdeman <2285702k@student.gla.ac.uk>
2018-02-23 12:36:05 +0000

    Adding the contacts of the teachers from Glasgow

--- assets/glasgow_teachers_scraper.py
+++ assets/glasgow_teachers_scraper.py
@@ -15,13 +15,22 @@ for school in soup.find('div',{'class':'maincontent'}).find('ul').find_all('a'):
 BASE_URL = "https://www.gla.ac.uk"
 STAFF_URL = "staff/"
 
-for school in schools:    
+for school in schools:
+        
     source_code = requests.get(BASE_URL+school['url']+STAFF_URL)
     soup = BeautifulSoup(source_code.text,'lxml')
     teachers = []
     for teacher in soup.find('ul',{'class':'longlist'}).find_all('li'):
         teachers.append(teacher.text)
+        # print(BASE_URL+teacher.a['href'])
+        teacher_source_code = requests.get(BASE_URL+teacher.a['href'])
+        teacher_soup = BeautifulSoup(teacher_source_code.text,'lxml')
+        try:
+            print([c.split(":") for c in filter(None,teacher_soup.find('div',{'id':'sp_contactInfo'}).text.split("\n"))])
+        except:
+            print(None)
     school['teachers']=teachers
+    # print(school)
     break
 
 
