9e35b3c236bf7458aa97fa70aaea1fc0a6a86717
hkdeman <2285702k@student.gla.ac.uk>
2018-03-04 18:42:22 +0000

    Added Glasgow University Teacher Data

--- assets/glasgow_subjects_scraper.py
+++ assets/glasgow_subjects_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from tqdm import tqdm
 
 #https://www.gla.ac.uk/schools/ take the subjects and their course teachers
 
@@ -10,17 +11,17 @@ soup = BeautifulSoup(source_code.text,'lxml')
 fields = []
 BASE_URL = "https://www.gla.ac.uk"
 
-for field in soup.find('div',{'class':'maincontent fullwidth'}).find_all('a'):
+for field in tqdm(soup.find('div',{'class':'maincontent fullwidth'}).find_all('a'),desc="Fields"):
     source_code = requests.get(BASE_URL+field['href'])
     soup = BeautifulSoup(source_code.text,'lxml')
     subjects = []
-    for year in soup.find('div',{'class':'maincontent fullwidth'}).find_all('ul'):
+    for year in tqdm(soup.find('div',{'class':'maincontent fullwidth'}).find_all('ul'),desc="Year"):
         for a in year.find_all('a'):
             subject_source_code = requests.get(BASE_URL+a['href'])
             soup = BeautifulSoup(subject_source_code.text,'lxml')
             desc = list(filter(None, [l.text for l in soup.find_all('p')]))[4]
             subjects.append({"url":a['href'],"title":a.text.strip(),"description":desc.strip()})
-    fields.append({"link":field['href'],"title":field.text,'subjects':subjects})
+    fields.append({"link":BASE_URL+field['href'],"title":field.text.strip(),'subjects':subjects})
 
-with open('glasgow_university_data.json', 'w') as outfile:
+with open('glasgow_university_subject_data.json', 'w') as outfile:
     json.dump(fields, outfile)
--- assets/glasgow_teachers_scraper.py
+++ assets/glasgow_teachers_scraper.py
@@ -1,6 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+import pip
 import json
+import time
+
+try:
+    import requests
+    from bs4 import BeautifulSoup 
+    from tqdm import tqdm       
+except(ImportError):
+    pip.main(['install',"requests","lxml","bs4","tqdm"])
+    import requests
+    from bs4 import BeautifulSoup 
+    from tqdm import tqdm
 
 #https://www.gla.ac.uk/schools/ take the subjects and their course teachers
 
@@ -10,29 +20,44 @@ soup = BeautifulSoup(source_code.text,'lxml')
 schools = []
 
 for school in soup.find('div',{'class':'maincontent'}).find('ul').find_all('a'):
-    schools.append({"title":school.text,"url":school['href']})
+   schools.append({"title":school.text,"url":school['href']})
 
 BASE_URL = "https://www.gla.ac.uk"
 STAFF_URL = "staff/"
 
-for school in schools:
-        
+all_contacts = {}
+
+
+for school in tqdm(schools,desc="Schools"):
+    if(schools.index(school)==14 or schools.index(school)==17):
+        continue 
     source_code = requests.get(BASE_URL+school['url']+STAFF_URL)
     soup = BeautifulSoup(source_code.text,'lxml')
-    teachers = []
-    for teacher in soup.find('ul',{'class':'longlist'}).find_all('li'):
-        teachers.append(teacher.text)
-        # print(BASE_URL+teacher.a['href'])
+    if not school['title'] in all_contacts.keys():
+        all_contacts[school['title']] = []
+    for teacher in tqdm(soup.find('ul',{'class':'longlist'}).find_all('li'),desc="teachers"):
         teacher_source_code = requests.get(BASE_URL+teacher.a['href'])
         teacher_soup = BeautifulSoup(teacher_source_code.text,'lxml')
+        has_image = True
+        image =  None
         try:
-            print([c.split(":") for c in filter(None,teacher_soup.find('div',{'id':'sp_contactInfo'}).text.split("\n"))])
+            image = BASE_URL+teacher_soup.find("div",{"id":"sp_staffphoto"}).find('img')['src']
         except:
-            print(None)
-    school['teachers']=teachers
-    # print(school)
-    break
+            has_image = False
+        
+        contact = None
+        try:
+            contact = [c.split(":") for c in filter(None,teacher_soup.find('div',{'id':'sp_contactInfo'}).text.split("\n"))]
+            if len(contact) ==2:            
+                all_contacts[school['title']].append({"name":teacher.a.text,"contact":{"email":contact[1][1].strip(),"mobile":contact[0][1].strip()},"image":image})
+            else:
+                all_contacts[school['title']].append({"name":teacher.a.text,"contact":{"mobile":contact[0][1].strip()},"image":image})                
+        except:
+            all_contacts[school['title']].append({"name":teacher.a.text,"contact":None,"image":image})
+#        time.sleep(5)
+    time.sleep(15)
 
-
+with open('glasgow_university_teacher_data.json', 'w') as outfile:
+    json.dump(all_contacts, outfile)
 
 
