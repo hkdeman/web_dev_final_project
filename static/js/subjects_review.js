63b929214e59e3c6c7947f136dc87bb08e323849
Gabriele Daugenaite <2267440d@student.gla.ac.uk>
2018-03-18 12:55:47 +0000

    Added rating for review

--- static/js/subjects_review.js
+++ static/js/subjects_review.js
@@ -1,4 +1,3 @@
-var rating=Math.floor(Math.random() * 5) + 1;
 var course;
 $(document).ready(function() {
     var str = window.location.href;
@@ -6,10 +5,18 @@ $(document).ready(function() {
     course = Number(str.substring(n + 1));
 });
 
+
 $('.btn-add-comment').click(function() {
     var textarea_styling= "<textarea id='text' style='width:100%;height:200px;padding:2%;font-family: 'Abel', sans-serif; \
-     line-height: 25px;border: solid 1px #ddd;'></textarea>";
-    swal({
+     line-height: 25px;border: solid 1px #ddd;'></textarea><br>\
+     <span> Add a rating </span> <br>\
+     <label class='radio-inline'><input type='radio' name='rateRadio' value='1'>1</label>\
+    <label class='radio-inline'><input type='radio' name='rateRadio' value='2'>2</label>\
+    <label class='radio-inline'><input type='radio' name='rateRadio' value='3'>3</label>\
+    <label class='radio-inline'><input type='radio' name='rateRadio' value='4'>4</label>\
+    <label class='radio-inline'><input type='radio' name='rateRadio' value='5'>5</label>";
+
+	swal({
         title: "Add Your Honest Opinion",
         text: textarea_styling,
         html: true,
@@ -17,19 +24,24 @@ $('.btn-add-comment').click(function() {
         closeOnConfirm: false,
         showLoaderOnConfirm: true,
         animation: "slide-from-top",
-      }, function() {
+     }, function() {
         var review = document.getElementById('text').value;
+
+        var rating = Number($("input[name='rateRadio']:checked").val());
+
         $.post('/add-review', {
-            what:"course",            
+            what:"course",
             course: course,
             review: review,
             rating: rating,
-            csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value
-			location.reload()
-        },function(data) {
-            if (data == "Error") {
-                swal("Error!", "The review could not be added", "error");
+            csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
+            },function(data) {            
+            if (data == "Success") {
+                swal.close();
+                location.reload();
+            } else {
+                swal("Error!", "The review could not be added", "error");                
             }
-        });
+        }, location.reload());
     });
 });
